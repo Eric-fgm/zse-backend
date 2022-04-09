8b8e723140fcff4696fe1a2dddedb8746867abd8
Eric-fgm <63963311+Eric-fgm@users.noreply.github.com>
2022-04-09 22:23:38 +0200

    fixes #3

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,9 +1,9 @@
 export default {
-  DB_HOST: process.env.DB_HOST || "localhost",
-  DB_USER: process.env.DB_USER || "root",
-  DB_PASSWORD: process.env.DB_PASSWORD || "",
+  DB_HOST: process.env.DB_HOST || "sql11.freesqldatabase.com",
+  DB_USER: process.env.DB_USER || "sql11484777",
+  DB_PASSWORD: process.env.DB_PASSWORD || "e8N13hsWpu",
   DB_PORT: process.env.DB_PORT || 3306,
-  DB_DATABASE: process.env.DB_DATABASE || "zse",
+  DB_DATABASE: process.env.DB_DATABASE || "sql11484777",
   DB_CONNECTION_LIMIT: process.env.MY_SQL_DB_CONNECTION_LIMIT
     ? parseInt(process.env.MY_SQL_DB_CONNECTION_LIMIT)
     : 4,
