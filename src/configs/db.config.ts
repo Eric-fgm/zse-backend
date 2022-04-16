b542851fd6ec6281d03632e4369203117561b696
Eric-fgm <63963311+Eric-fgm@users.noreply.github.com>
2022-04-16 22:35:59 +0200

    fix db

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,5 +1,5 @@
 export default {
-  DB_HOST: process.env.DB_HOST || "sql11.freesqldatabase.com",
+  DB_HOST: process.env.DB_HOST || "db4free.net",
   DB_USER: process.env.DB_USER || "sql11484777",
   DB_PASSWORD: process.env.DB_PASSWORD || "e8N13hsWpu",
   DB_PORT: process.env.DB_PORT || 3306,
